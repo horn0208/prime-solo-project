469dbbf1a9cc3d1dafca676f92eb9547ae364dec
Liz Horn <liz.mpls@gmail.com>
2022-07-15 12:00:51 -0500

    area details template page and hashrouting

---
+++ src/components/AreaDetails/AreaDetails.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+
+function AreaDetails(){
+    // template hook
+    const [ hook, setHook ] = useState( null );
+     
+    return(
+        <div>
+            <h2>AreaDetails</h2>
+        </div>
+    );
+}
+
+export default AreaDetails;
