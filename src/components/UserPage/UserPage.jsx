db15007078b889d21b8fe6d2e449f196d0a79ce8
Liz Horn <liz.mpls@gmail.com>
2022-07-15 08:43:45 -0500

    saga is fetching areas from db

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import LogOutButton from '../LogOutButton/LogOutButton';
-import {useSelector} from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux';
 
 function UserPage() {
-  // this component doesn't do much to start, just renders some user reducer info to the DOM
+  // this component is the user welcome page and climbing area list view
   const user = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    dispatch({type: 'FETCH_ALL_AREAS'});
+  }, []);
+
   return (
     <div className="container">
       <h2>Welcome, {user.username}!</h2>
-      <p>Your ID is: {user.id}</p>
-      <LogOutButton className="btn" />
+      <p>Where do you want to climb?</p>
+      {/* <p>Your ID is: {user.id}</p> */}
+      {/* <LogOutButton className="btn" /> */}
+
+      {/* map over areas and display */}
+
     </div>
   );
 }
