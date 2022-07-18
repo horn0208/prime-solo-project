88ccff206372fd88d683af89b3238310255cc6b9
Liz Horn <liz.mpls@gmail.com>
2022-07-18 16:29:04 -0500

    about page layout and copy

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import './AboutPage.css';
+import Typography from '@mui/material/Typography';
 
 // This is one of our simplest components
 // It doesn't have local state,
@@ -7,10 +9,28 @@ import React from 'react';
 
 function AboutPage() {
   return (
-    <div className="container">
-      <div>
-        <p>This about page is for anyone to read!</p>
+    <div className="mega-container">
+        <Typography variant='h5'>Is The Rock Wet?</Typography>
+        
+      <div className='info'>
+        <p>For rock climbers, it’s hard to figure out whether the rock 
+          in an area is likely to be dry and safe to climb without driving 
+          there, often several hours away, to check. Even worse, sometimes 
+          unknown factors can ruin your day when you arrive unprepared, like 
+          breaking trail through waist-deep snow on the hike in, a sudden hatch of 
+          biting flies, or finding that falcons are nesting on the route you 
+          planned to climb.
+          <br />
+          <br />
+          WRDR gives climbers an easy way to check weather conditions and recent 
+          rainfall where they hope to climb, as well as a place to share and view 
+          other climbers’ recent experiences so there are fewer unknowns before 
+          committing to that long drive.
+          <br />
+          <br />
+        </p>
       </div>
+      <Typography variant='button'>Conditions are weird. <br /> You should get WRDR.</Typography>
     </div>
   );
 }
